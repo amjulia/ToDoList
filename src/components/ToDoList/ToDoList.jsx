f5803a4484876281e0381866f07e57f69a0f9d84
Karpova Julia <am_julia@mail.ru>
2024-10-18 17:51:28 +0600

    готово

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,69 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   editTodo,
   removeTodo,
+  selectFilteredTodos,
   toggleTodo,
 } from "../../store/features/todoSlice";
 import styles from "./ToDoList.module.css";
 import cn from "classnames";
+import Filter from "../Filter/Filter";
 
 const TodoList = () => {
-  const todos = useSelector((state) => state.todos);
+  const todos = useSelector(selectFilteredTodos);
   const dispatch = useDispatch();
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [newText, setNewText] = useState("");
 
+  const handleEdit = (id) => {
+    if (newText) {
+      dispatch(editTodo({ id, text: newText }));
+      setEditingTodoId(null);
+      setNewText("");
+    }
+  };
   return (
     <div className={styles.content}>
-      <div className={styles.filter}>
-        <div className={cn(styles.filter__btn, styles.btn1)}>Все</div>
-        <div className={cn(styles.filter__btn, styles.btn2)}>Выполнено</div>
-        <div className={cn(styles.filter__btn, styles.btn3)}>Не выполнено</div>
-      </div>
-      <div className={styles.task}>
-        <h3>Сегодня</h3>
-        <div className={styles.taskBox}>
-                    <svg className={styles.task__icon}
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="10" cy="10" r="9"  stroke-width="2" />
-          </svg>
-          <p>Завершить написание SEO-текста с учетом всех требований.</p>
-        </div>
-        <div className={styles.svgBox}>
-          <svg className={styles.btn__icon}>
-            <use xlinkHref="/sprite.svg#edit"></use>
-          </svg>
-          <svg className={styles.btn__delete}>
-            <use xlinkHref="/sprite.svg#delete"></use>
-          </svg>
-          
-        </div>
-      </div>
+      <Filter />
+      {todos.map((todo) => (
+        <div key={todo.id} className={styles.task}>
+          <h3>Сегодня</h3>
+          <div className={styles.taskBox}>
+            <svg
+              onClick={() => dispatch(toggleTodo(todo.id))}
+              className={cn(styles.task__icon, {
+                [styles.task__iconActive]: todo.completed,
+              })}
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="10" cy="10" r="9" strokeWidth="2" />
+            </svg>
 
-      {/* {todos.map((todo) => (
-        <div key={todo.id}>
-          <input
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => dispatch(toggleTodo(todo.id))}
-          />
-          <span>{todo.text}</span>
-          <button onClick={() => dispatch(removeTodo(todo.id))}>Удалить</button>
-          <button
-            onClick={() => {
-              const newText = prompt("Редактировать задачу:", todo.text);
-              if (newText) {
-                dispatch(editTodo({ id: todo.id, text: newText }));
-              }
-            }}
-          >
-            Редактировать
-          </button>
+            <input
+              onChange={(e) => setNewText(e.target.value)}
+              className={cn(styles.task__input, styles.task__text, {
+                [styles.textCrossed]: todo.completed,
+                [styles.task__input_edit]: editingTodoId === todo.id,
+              })}
+              type="text"
+              defaultValue={editingTodoId === todo.id ? newText : todo.text}
+              disabled={editingTodoId !== todo.id}
+            />
+          </div>
+          <div className={styles.svgBox}>
+            {editingTodoId === todo.id && (
+              <button
+                className={styles.button__edit}
+                onClick={() => handleEdit(todo.id)}
+              >
+                Сохранить
+              </button>
+            )}
+            <div
+              onClick={() => {
+                setEditingTodoId(editingTodoId === todo.id ? null : todo.id);
+                setNewText(todo.text);
+              }}
+            >
+              <svg
+                className={cn(styles.btn__icon, {
+                  [styles.btn__iconActive]: editingTodoId === todo.id,
+                })}
+              >
+                <use xlinkHref="/sprite.svg#edit"></use>
+              </svg>
+            </div>
+
+            <svg
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className={styles.btn__delete}
+            >
+              <use xlinkHref="/sprite.svg#delete"></use>
+            </svg>
+          </div>
         </div>
-      ))} */}
+      ))}
+
     </div>
   );
 };
--- src/store/features/todoSlice.js
+++ src/store/features/todoSlice.js
@@ -2,28 +2,49 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
   name: "todos",
-  initialState: [{ id: Date.now(), text: "Выпить кофе", completed: false }],
+  initialState: {
+    todos: [{ id: Date.now(), text: "Выпить кофе", completed: false }],
+    filter: "all",
+  },
   reducers: {
     addTodo: (state, action) => {
-      state.push({ id: Date.now(), text: action.payload, completed: false });
+      state.todos.push({
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      });
     },
     toggleTodo: (state, action) => {
-      const todo = state.find((todo) => todo.id === action.payload);
+      const todo = state.todos.find((todo) => todo.id === action.payload);
       if (todo) {
         todo.completed = !todo.completed;
       }
     },
     removeTodo: (state, action) => {
-      return state.filter((todo) => todo.id !== action.payload);
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+    
     editTodo: (state, action) => {
-      const todo = state.find((todo) => todo.id === action.payload.id);
+      const todo = state.todos.find((todo) => todo.id === action.payload.id);
       if (todo) {
         todo.text = action.payload.text;
       }
     },
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
   },
 });
-
-export const { addTodo, toggleTodo, removeTodo, editTodo } = todoSlice.actions;
+export const selectFilteredTodos = (state) => {
+  switch (state.todos.filter) {
+    case "completed":
+      return state.todos.todos.filter((todo) => todo.completed);
+    case "not_completed":
+      return state.todos.todos.filter((todo) => !todo.completed);
+    default:
+      return state.todos.todos;
+  }
+};
+export const { addTodo, toggleTodo, removeTodo, editTodo, setFilter } =
+  todoSlice.actions;
 export default todoSlice.reducer;
